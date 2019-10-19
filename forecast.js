072e3b4bef80e7e5f7966e90ca9164a356558b14
Aegontar <38143858+Aegontar@users.noreply.github.com>
2019-10-20 01:41:48 +0600

    Update forecast.js
    I have changed the http to https

--- forecast.js
+++ forecast.js
@@ -4,7 +4,7 @@ const key = 'WfSVtF0oFe0JmBKEDyt0CAD5tuTUH8q6';
 //get weather information
 const getWeather = async (id) => {
 
-const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
 const query =  `${id}?apikey=${key}`;
 
 const response = await fetch(base + query);
@@ -16,7 +16,7 @@ return data[0];
 
 const getCity = async (city) => {
 
-const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
 const query =  `?apikey=${key}&q=${city}`;
 
 const response = await fetch(base + query);
